c4f174602aa6b5c24d13083ea17021d0d054a9c7
Giorgos Logiotatidis <seadog@sealabs.net>
2015-11-11 15:54:48 +0200

    Default to snippets.m.c

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 set -xe
 PREFS_PATH="/root/.mozilla/firefox/snippets.default/prefs.js"
 
-export SNIPPET_HOST=${SNIPPET_HOST:-"http://127.0.0.1:8000"}
+export SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
 export SNIPPET_HOST_NO_PROTOCOL=`echo $ $SNIPPET_HOST | cut -d / -f 3`
 
 cat $PREFS_PATH | envsubst | sponge $PREFS_PATH
