1735559175ec090a6980295b589131c1b7e96998
Giorgos Logiotatidis <seadog@sealabs.net>
2015-11-12 16:20:22 +0200

    Remove trailing slash.

--- bin/run.sh
+++ bin/run.sh
@@ -6,6 +6,6 @@ then
 fi;
 
 VERSION=${1:-42.0}
-SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com/"}
+SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
 
 docker run -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e SNIPPET_HOST=$SNIPPET_HOST firefox-${VERSION}
