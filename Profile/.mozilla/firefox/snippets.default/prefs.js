63d5ee2ed41e8c6b20b3006c3eef94031a72f6f9
Giorgos Logiotatidis <seadog@sealabs.net>
2015-11-13 12:59:46 +0200

    Add custom snippet_product_name

--- Profile/.mozilla/firefox/snippets.default/prefs.js
+++ Profile/.mozilla/firefox/snippets.default/prefs.js
@@ -1,6 +1,6 @@
 # Mozilla User Preferences
 
-user_pref("browser.aboutHomeSnippets.updateUrl", "$SNIPPET_HOST/%STARTPAGE_VERSION%/%NAME%/%VERSION%/%APPBUILDID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/");
+user_pref("browser.aboutHomeSnippets.updateUrl", "$SNIPPET_HOST/%STARTPAGE_VERSION%/$SNIPPET_PRODUCT_NAME/%VERSION%/%APPBUILDID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/");
 user_pref("browser.uitour.requireSecure", false);
 // Firefox >= 36
 user_pref("browser.uitour.testingOrigins", "$SNIPPET_HOST, https://snippets-dev.allizom.org, https://snippets.allizom.org, https://snippets.mozilla.org");
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@ set -xe
 PREFS_PATH="/root/.mozilla/firefox/snippets.default/prefs.js"
 
 export SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
-export SNIPPET_HOST_NO_PROTOCOL=`echo $ $SNIPPET_HOST | cut -d / -f 3`
+export SNIPPET_HOST_NO_PROTOCOL=`echo $SNIPPET_HOST | cut -d / -f 3`
+export SNIPPET_PRODUCT_NAME=${SNIPPET_PRODUCT_NAME:-"%NAME%"}
 
 cat $PREFS_PATH | envsubst | sponge $PREFS_PATH
 
