b70496c6e65d825f814af384bbb293e01fe9ffcf
Giorgos Logiotatidis <seadog@sealabs.net>
2016-01-14 12:02:01 +0200

    Update to firefox 43.0

--- bin/run.sh
+++ bin/run.sh
@@ -5,8 +5,8 @@ then
     . ./bin/local_variables.sh
 fi;
 
-VERSION=${1:-42.0}
+VERSION=${1:-43.0}
 SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
 SNIPPET_PRODUCT_NAME=${SNIPPET_PRODUCT_NAME:-"%NAME%"}
 
-docker run -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e SNIPPET_HOST=$SNIPPET_HOST -e SNIPPET_PRODUCT_NAME firefox-${VERSION}
+docker run -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e SNIPPET_HOST=$SNIPPET_HOST -e SNIPPET_PRODUCT_NAME giorgos/firefox-${VERSION}
