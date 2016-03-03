44ecc4bcd0c2fa36882c00ec93042b74e172857f
Giorgos Logiotatidis <seadog@sealabs.net>
2016-03-03 11:21:53 +0200

    Correct docker image name.

--- bin/run.sh
+++ bin/run.sh
@@ -9,4 +9,4 @@ VERSION=${1:-43.0}
 SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
 SNIPPET_PRODUCT_NAME=${SNIPPET_PRODUCT_NAME:-"%NAME%"}
 
-docker run -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e SNIPPET_HOST=$SNIPPET_HOST -e SNIPPET_PRODUCT_NAME giorgos/firefox-${VERSION}
+docker run -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e SNIPPET_HOST=$SNIPPET_HOST -e SNIPPET_PRODUCT_NAME giorgos/fx-docker:firefox-${VERSION}
