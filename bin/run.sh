194791e89d8d865b3a7e5128f36638cfacb4fb3b
Giorgos Logiotatidis <seadog@sealabs.net>
2016-10-24 11:06:01 +0300

    Upgrade to Firefox 49.0

--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ then
     . ./bin/local_variables.sh
 fi;
 
-VERSION=${1:-48.0}
+VERSION=${1:-49.0}
 SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
 SNIPPET_PRODUCT_NAME=${SNIPPET_PRODUCT_NAME:-"%NAME%"}
 
