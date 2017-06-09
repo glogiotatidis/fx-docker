de3d3962b0b2e47ab165531b69595f91d3f8004c
Giorgos Logiotatidis <seadog@sealabs.net>
2017-06-09 16:26:46 +0300

    Run xhost before and after.

--- bin/run.sh
+++ bin/run.sh
@@ -5,8 +5,11 @@ then
     . ./bin/local_variables.sh
 fi;
 
-VERSION=${1:-50.0}
+VERSION=${1:-53.0}
 SNIPPET_HOST=${SNIPPET_HOST:-"https://snippets.mozilla.com"}
 SNIPPET_PRODUCT_NAME=${SNIPPET_PRODUCT_NAME:-"%NAME%"}
 
+# Must make this specific to the docker container launched
+xhost +
 docker run --net=host -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e SNIPPET_HOST=$SNIPPET_HOST -e SNIPPET_PRODUCT_NAME giorgos/fx-docker:firefox-${VERSION}
+xhost -
